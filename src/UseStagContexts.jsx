0e9a575609d4e7f6f2eae166d05e3f1e3446ff99
techstackmedia <31328325+techstackmedia@users.noreply.github.com>
2022-10-06 04:43:48 -0700

    Update UseStagContexts.jsx

--- src/UseStagContexts.jsx
+++ src/UseStagContexts.jsx
@@ -3,32 +3,24 @@ import StateContexts from "./context/StageContexts";
 
 const UseStageContexts = () => {
   const { state, fetchAPI } = useContext(StateContexts);
-
-  let sortNames = [];
-  const handleClick = () => {
-    sortNames = state
-      .sort((a, b) => {
-        return a.name > b.name ? 1 : -1;
-      })
-      .map((item) => {
-        return item.name;
-      });
-    console.log(sortNames);
-    setSortItems(sortNames);
-    return sortNames;
-  };
-
-  const [sortItems, setSortItems] = useState([]);
-  console.log("sort", sortItems);
   useEffect(() => {
     fetchAPI();
-  }, [sortItems]);
+  }, []);
+  const [sortItems, setSortItems] = useState([]);
+
+  let sortNames = [...state];
+  const handleClick = () => {
+    sortNames = state.sort((a, b) => {
+      return a.name > b.name ? 1 : -1;
+    });
+    setSortItems(sortNames);
+  };
 
   return (
     <>
       <button onClick={handleClick}>Click Me</button>
       <div>
-        {sortItems.map((item, index) => {
+        {sortNames.map((item, index) => {
           return <div key={index}>{item.name}</div>;
         })}
       </div>
