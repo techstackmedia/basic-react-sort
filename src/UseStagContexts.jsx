6d86c60fdd364aa398fefc4a50bc385171107cb4
techstackmedia <31328325+techstackmedia@users.noreply.github.com>
2022-10-07 12:23:10 -0700

    Update UseStagContexts.jsx

--- src/UseStagContexts.jsx
+++ src/UseStagContexts.jsx
@@ -8,21 +8,39 @@ const UseStageContexts = () => {
     fetchAPI();
   }, []);
 
-  let sortNames = [...state];
-  const handleClick = () => {
-    sortNames = state.sort((a, b) => {
+  let data = [...state];
+  const handleAscendingClick = () => {
+    const sortNames = [...state].sort((a, b) => {
       return a.name > b.name ? 1 : -1;
+    });
+    setSortItems(sortNames);
+  };
+
+  const handleResetClick = () => {
+    setSortItems(state);
+  };
+
+  const handleDecendingClick = () => {
+    const sortNames = [...state].sort((a, b) => {
+      return a.name > b.name ? -1 : 1;
     });
     setSortItems(sortNames);
   };
 
   return (
     <>
-      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleAscendingClick}>Ascending</button>
+      <button onClick={handleResetClick}>Reset</button>
+      <button onClick={handleDecendingClick}>Decending</button>
+
       <div>
-        {sortNames.map((item, index) => {
-          return <div key={index}>{item.name}</div>;
-        })}
+        {sortItems.length === 0
+          ? data.map((item, index) => {
+              return <div key={index}>{item.name}</div>;
+            })
+          : sortItems.map((item, index) => {
+              return <div key={index}>{item.name}</div>;
+            })}
       </div>
     </>
   );
